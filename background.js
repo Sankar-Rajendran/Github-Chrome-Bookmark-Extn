a7554c83b732692501faaadf0871d9461822d72d
sankar.rajendran <sankar.rajendran@imaginea.com>
2018-06-01 12:11:05 +0530

    first commit

---
+++ background.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var ownerNameElement, repoNameElement, fileNameElement, accessTokenElement;
+
+    ownerNameElement = document.getElementById('owner_name_txt');
+    repoNameElement = document.getElementById('repo_name_txt');
+    fileNameElement = document.getElementById('file_name_txt');
+    accessTokenElement = document.getElementById('access_token_txt');
+
+    ownerNameElement.value = localStorage.getItem("owner_name");
+    repoNameElement.value = localStorage.getItem("repo_name");
+    fileNameElement.value = localStorage.getItem("file_name");
+    accessTokenElement.value = localStorage.getItem("access_token");
+
+
+    var clearFields = (fromButton) => {
+        ownerNameElement.value = repoNameElement.value = fileNameElement.value = accessTokenElement.value = '';
+
+        if (fromButton) {
+            localStorage.removeItem('owner_name');
+            localStorage.removeItem('repo_name');
+            localStorage.removeItem('file_name');
+            localStorage.removeItem('access_token');
+        }
+    }
+
+
+    var clearButton = document.getElementById('clear_fields');
+
+    clearButton.addEventListener('click', function () {
+        clearFields(true);
+    });
+
+
+    var saveChangesButton = document.getElementById('save_changes');
+
+    saveChangesButton.addEventListener('click', function () {
+        var ownerName = ownerNameElement.value;
+        var repoName = repoNameElement.value;
+        var fileName = fileNameElement.value;
+        var accessToken = accessTokenElement.value;
+
+        localStorage.setItem("owner_name", ownerName);
+        localStorage.setItem("repo_name", repoName);
+        localStorage.setItem("file_name", fileName);
+        localStorage.setItem("access_token", accessToken);
+
+        clearFields(false);
+
+        alert('Your changes are saved successfully');
+    })
+
+});
+
---
+++ popup.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    var saveButton = document.getElementById('save_link');
+
+    //var saveButton = $('#save_link');
+
+
+
+    function saveURL(url) {
+
+
+        var ownerName = localStorage.getItem("owner_name");
+        var repoName = localStorage.getItem("repo_name");
+        var fileName = localStorage.getItem("file_name");
+        var accessToken = localStorage.getItem("access_token");
+
+        if (!((ownerName !== null && ownerName !== '') &&
+            (repoName !== null && repoName !== '') &&
+            (fileName !== null && fileName !== '') &&
+            (accessToken !== null && accessToken !== ''))) {
+            chrome.tabs.create({ 'url': chrome.extension.getURL('change_settings.html') });
+            return false;
+        }
+
+        var newLink = '[' + url + ']';
+
+        var saveButton1 = $('#save_link');
+
+        $.ajax({
+            url: `https://api.github.com/repos/${ownerName}/${repoName}/contents/${fileName}`,
+            success: function (results) {
+                var sha, updateContent;
+                var content = results.content;
+                sha = results.sha;
+                var decode = atob(content);
+                updateContent = decode + '\n' + newLink + '\n';
+
+                var dataObject = {
+                    'message': `A link ${newLink} has been added`,
+                    'content': btoa(updateContent),
+                    'sha': sha
+                };
+
+                $.ajax({
+                    url: `https://api.github.com/repos/${ownerName}/${repoName}/contents/${fileName}`,
+                    type: 'PUT',
+                    data: JSON.stringify(dataObject),
+                    success: function (result) {
+                        console.log(result);
+                    },
+                    beforeSend: function (xhr) {
+                        xhr.setRequestHeader('Authorization', 'BEARER ' + accessToken);
+                    },
+                    error: function (error) {
+                        console.log(error);
+                    }
+                });
+
+            }
+        });
+    }
+
+
+
+
+
+
+    saveButton.addEventListener('click', function (event) {
+        var tablink;
+        chrome.tabs.getSelected(null, function (tab) {
+            saveURL(tab.url);
+        });
+    });
+
+    var changeButton = document.getElementById('change_settings');
+
+    changeButton.addEventListener('click', function () {
+        chrome.tabs.create({ 'url': chrome.extension.getURL('change_settings.html') });
+        return false;
+    });
+
+
+});
